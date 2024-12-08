d0a9ad77376b8d176025b3013f4041e16c3920e4
SiddhishNirgude <99582612+SiddhishNirgude@users.noreply.github.com>
2024-12-08 02:26:13 -0500

    Update streamlit_final.py

--- streamlit_final.py
+++ streamlit_final.py
@@ -42,7 +42,7 @@ merged_dataset = load_dataset("final_integrated_dataset.csv")
 insurance_encoded = load_dataset("insurance_encoded.csv")
 features_encoded = load_dataset("features_encoded.csv")
 maintenance_encoded = load_dataset("maintenance_encoded.csv")
-
+final_integrated_dataset = load_dataset("final_integrated_dataset.csv.csv")
 
 # Check for dataset loading errors and show relevant messages
 if car_insurance_claim is None:
@@ -73,7 +73,8 @@ if features_encoded is None:
     st.error("Error: Encoded Features Data could not be loaded. Please check the file path.")
 if maintenance_encoded is None:
     st.error("Error: Encoded Maintenance Data could not be loaded. Please check the file path.")
-
+if final_integrated_dataset is None:
+    st.error("Error: Final Integrated Data could not be loaded. Please check the file path.")
 
 
 # --- TOP-LEVEL NAVIGATION ---
